bab0ec2bf778702b839c8e35342688d4d1d9ed35
olankens <173156207+olankens@users.noreply.github.com>
2025-12-08 18:50:54 +0100

    Remove unused assets and license

--- src/msgstyle.sh
+++ src/msgstyle.sh
@@ -1,1 +1,41 @@
+assert_lenght_between_12_and_72() {
 
+    return 1
+
+}
+
+assert_no_scope_or_type() {
+
+    return 1
+
+}
+
+assert_no_trailing_punctuation() {
+
+    return 1
+
+}
+
+assert_no_weird_characters() {
+
+    return 1
+
+}
+
+assert_start_with_allowed_verb() {
+
+    return 1
+}
+
+main() {
+
+    assert_lenght_between_12_and_72 "$@"
+    assert_no_scope_or_type "$@"
+    assert_no_trailing_punctuation "$@"
+    assert_no_weird_characters "$@"
+    assert_start_with_allowed_verb "$@"
+    return 1
+
+}
+
+main "$@"
