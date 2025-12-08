93de860b5c7026cb67d087c3e4170fc2d6e24362
olankens <173156207+olankens@users.noreply.github.com>
2025-12-08 19:24:13 +0100

    Create message style validators

--- src/msgstyle.sh
+++ src/msgstyle.sh
@@ -1,41 +1,78 @@
+#!/bin/bash
+
+# shellcheck shell=bash
+# shellcheck disable=SC2034,SC2155
+
 assert_lenght_between_12_and_72() {
 
-    return 1
+    local payload=$(head -n 1 "$1")
+
+    if [[ ${#payload} -ge 12 && ${#payload} -le 72 ]]; then return 0; fi
+
+    echo "❌ Commit message must be between 12 and 72 characters long"
+    echo "Your message is: '$payload'"
+    exit 1
 
 }
 
 assert_no_scope_or_type() {
 
-    return 1
+    local payload=$(head -n 1 "$1")
+
+    if [[ "$payload" != *"["* && "$payload" != *"]"* ]]; then return 0; fi
+
+    echo "❌ Commit message must not contain scope or type"
+    echo "Your message is: '$payload'"
+    exit 1
 
 }
 
 assert_no_trailing_punctuation() {
 
-    return 1
+    local payload=$(head -n 1 "$1")
+
+    if [[ "$payload" != *"."* && "$payload" != *","* && "$payload" != *":"* ]]; then return 0; fi
+
+    echo "❌ Commit message must not contain trailing punctuation"
+    echo "Your message is: '$payload'"
+    exit 1
 
 }
 
 assert_no_weird_characters() {
 
-    return 1
+    local payload=$(head -n 1 "$1")
+    local pattern='[@#$%^&*(){}[\]<>|\\~]'
+
+    if [[ ! "$payload" =~ $pattern ]]; then return 0; fi
+
+    echo "❌ Commit message must not contain special characters"
+    echo "Your message is: '$payload'"
+    exit 1
 
 }
 
 assert_start_with_allowed_verb() {
 
-    return 1
+    local payload=$(head -n 1 "$1" | awk '{print $1}' | sed 's/[:,]$//')
+
+    local allowed=("Accept" "Adjust" "Bundle" "Bypass" "Cancel" "Concat" "Create" "Decode" "Delete" "Deploy" "Enable" "Encode" "Ensure" "Export" "Filter" "Forbid" "Format" "Freeze" "Harden" "Ignore" "Import" "Insert" "Loosen" "Permit" "Rebase" "Refine" "Reject" "Resume" "Return" "Revert" "Reword" "Secure" "Submit" "Switch" "Unpack" "Update" "Vanish" "Verify")
+    for element in "${allowed[@]}"; do [[ "$payload" == "$element" ]] && exit 0; done
+
+    echo "❌ Commit message must start with an allowed verb: ${allowed[*]}"
+    echo "Your message starts with: '$payload'"
+    exit 1
+
 }
 
 main() {
 
-    assert_lenght_between_12_and_72 "$@"
-    assert_no_scope_or_type "$@"
-    assert_no_trailing_punctuation "$@"
-    assert_no_weird_characters "$@"
-    assert_start_with_allowed_verb "$@"
-    return 1
+    assert_lenght_between_12_and_72 "$1"
+    assert_no_scope_or_type "$1"
+    assert_no_trailing_punctuation "$1"
+    assert_no_weird_characters "$1"
+    assert_start_with_allowed_verb "$1"
 
 }
 
-main "$@"
+main "$1"
